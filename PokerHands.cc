fe59c672d4219c767823f318d726f5bd9bd80190
jkoutsourais <jkoutsourais13@gmail.com>
2017-10-02 10:05:54 -0400

    Update PokerHands.cc

--- PokerHands.cc
+++ PokerHands.cc
@@ -49,12 +49,38 @@ int main (void)
 	else if (val1 == val4 || val2 == val5)
 		cout << "Four of a kind" << endl;
 
+//full house = (v1=v3 AND v4=v5) OR (v1=v2 AND v3=v5)
 
+	else if ((val1 == val3 && val4 == val5) || (val1 == val2 && val3 == val5))
+		 cout << "Full house" << endl;
 
+//flush = All c_n/13 are equal
+	
+	else if	((card1/13) == (card2/13) && (card2/13) == (card3/13) && (card3/13) == (card4/13) && (card4/13) == (card5/13))
+		cout << "Flush" << endl;
+		 
+//straight = (val1+4 = val5)
 
-
-
-
+	else if (val1+4 == val5)
+		 cout << "Straight" << endl;
+		 
+//three of a kind = (val1 = val3 OR val2=val4 OR val3=val5)
+	
+	else if (val1 == val3 || val2 == val4 || val3 == val5)
+		 cout << "Three of a kind" << endl;
+		 
+//two pair = (val1=val2 AND val3=val4) OR (val2=val3 AND val4=val5) OR (val1=val2 AND val4=val5)
+		
+	else if ((val1 == val2 && val3 == val4) || (val2 == val3 && val4 == val5) || (val1 == val2 && val4 == val5))
+		cout << "Two pair" << endl;
+		 
+//one pair = v1 == v2 OR v2 == v3 OR v3 == v4 OR v4 == v5
+	
+	else if (val1 == val2 || val2 == val3 || val3 == val4 || val4 == val5)
+		cout << "One pair" << endl;
+	
+	else
+		 cout << "No significant combination" << endl;
 
 return 0;
 }
